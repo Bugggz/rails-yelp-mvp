70bbc30858dffe01c1264910a3573e3d48156347
Benjamin Barbier <benjamin.barbier@gmail.com>
2020-11-11 16:39:44 +0100

    rails 3

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,10 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  CATEGORIES = ['chinese', 'japanese', 'italian', 'french', 'belgian']
+
+  validates :name, presence: true
+  validates :phone_number, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+balto = Restaurant.create({name: 'Le Balto', Address: 'Lille', phone_number: '0346723548', category: 'french' })
+pekin = Restaurant.create({name: 'Le Palais de Pekin', Address: 'Paris', phone_number: '0156987283', category: 'chinese' })
+tokyo = Restaurant.create({name: 'Tokyo Hotel', Address: 'Lyon', phone_number: '0237584526', category: 'Japanese' })
+fricabelle = Restaurant.create({name: 'La Fricabelle', Address: 'Tournais', phone_number: '0338976530', category: 'belgian' })
+bellavita = Restaurant.create({name: 'Bella Vita', Address: 'Brest', phone_number: '0385672841', category: 'Italian' })
